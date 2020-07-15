68ba4fc4c2bbc1d514b99c0115325cb15ffb002c
GVabal <gvabal@gmail.com>
2020-07-15 21:25:51 +0300

    Initial Commit

---
+++ src/main/java/Controller.java
@@ -0,0 +1,110 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Controller {
+
+    IBANCountryValidator validator = new IBANCountryValidator();
+
+    @FXML
+    private TextField filePathField;
+
+    @FXML
+    private TextField validatorField;
+
+    @FXML
+    private Text batchMessageText;
+
+    @FXML
+    private Text validatorText;
+
+    @FXML
+    private Rectangle tickLeft;
+
+    @FXML
+    private Rectangle tickRight;
+
+    @FXML
+    public void showTick() {
+        tickLeft.setFill(Color.GREEN);
+        tickRight.setFill(Color.GREEN);
+    }
+
+    @FXML
+    public void hideTick() {
+        tickLeft.setFill(Color.valueOf("#b1b1b1"));
+        tickRight.setFill(Color.valueOf("#b1b1b1"));
+    }
+
+    @FXML
+    public void validate() {
+        try {
+            validator.validateIBAN(validatorField.getText().replace(" ",""));
+            validatorText.setText("");
+            showTick();
+        } catch (Exception e) {
+            hideTick();
+            validatorText.setText(e.getMessage());
+        }
+    }
+
+    @FXML
+    public void setSelectFile() {
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Text files (*.txt)", "*.txt");
+        fileChooser.getExtensionFilters().add(extFilter);
+        File file = fileChooser.showOpenDialog(null);
+        if (file != null)
+        filePathField.setText(file.toString());
+    }
+
+    @FXML
+    public void processBatchValidation() {
+        List<String> lines = new ArrayList<>();
+
+        try {
+            lines = Files.readAllLines(Paths.get(filePathField.getText()));
+        } catch (Exception e) {
+            batchMessageText.setFill(Color.RED);
+            batchMessageText.setText(e.getMessage());
+        }
+
+        List<String> processedLines = lines.stream()
+                .map(line -> {
+                    try {
+                        line += ";" + validator.validateIBAN(line);
+                    } catch (Exception e) {
+                        line += ";false";
+                    }
+                    return line;
+                })
+                .collect(Collectors.toList());
+
+            String outputPath = "";
+            if (!filePathField.toString().equals(""))
+            outputPath = filePathField.getText().replaceFirst("\\.txt$", "") + ".out.txt";
+        try {
+            if (outputPath.equals(".out.txt"))
+                throw new RuntimeException("No file selected");
+            Files.write(Paths.get(outputPath), processedLines);
+            batchMessageText.setFill(Color.GREEN);
+            batchMessageText.setText("Output saved to " + outputPath);
+        } catch (Exception e) {
+            batchMessageText.setFill(Color.RED);
+            batchMessageText.setText(e.getMessage());
+        } finally {
+            filePathField.clear();
+        }
+    }
+
+}
---
+++ src/test/java/IBANCountryValidatorTest.java
@@ -0,0 +1,94 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IBANCountryValidatorTest {
+
+    static IBANCountryValidator validator;
+
+    @BeforeAll
+    static void beforeAll() {
+        validator = new IBANCountryValidator();
+    }
+
+    @Test
+    void testValidateFormat() {
+        assertTrue(validator.validateFormat("MD24AG000225100013104168"));
+        assertTrue(validator.validateFormat("ST68000100010051840112"));
+        assertTrue(validator.validateFormat("FR142"));
+        assertFalse(validator.validateFormat("Not an IBAN number"));
+        assertFalse(validator.validateFormat("5000400440116243"));
+        assertFalse(validator.validateFormat("DKXX00400440116243"));
+        assertFalse(validator.validateFormat("AAAA"));
+        assertFalse(validator.validateFormat("AA01"));
+    }
+
+    @Test
+    void testValidateCountry() {
+        assertTrue(validator.validateCountry("LT0000000"));
+        assertTrue(validator.validateCountry("SE0000000"));
+        assertFalse(validator.validateCountry("00SE0000000"));
+        assertFalse(validator.validateCountry("ZZ0000000"));
+    }
+
+    @Test
+    void testValidateLength() {
+        assertTrue(validator.validateLength("IE29AIBK93115212345678"));
+        assertTrue(validator.validateLength("KW81CBKU0000000000001234560101"));
+        assertFalse(validator.validateLength("IE29AIBK93112345678"));
+        assertFalse(validator.validateLength("IE29AIBK931152123654845678"));
+    }
+
+    @Test
+    void testValidateStructure() {
+        assertTrue(validator.validateStructure("BH67BMAG00001299123456"));
+        assertTrue(validator.validateStructure("TR330006100519786457841326"));
+        assertFalse(validator.validateStructure("XK0512D20123A5678906"));
+        assertFalse(validator.validateStructure("BY13N11B3600900000002Z00AB00"));
+    }
+
+    @Test
+    void testValidateMod97() {
+        assertTrue(validator.validateMod97("BE68539007547034"));
+        assertTrue(validator.validateMod97("IT60X0542811101000000123456"));
+        assertFalse(validator.validateMod97("BE68539607547032"));
+        assertFalse(validator.validateMod97("IT60X0542811501000700123456"));
+    }
+
+    @Test
+    void testValidateIBAN() {
+        assertTrue(validator.validateIBAN("MK07250120000058984"));
+
+        Throwable e1 = assertThrows(
+                RuntimeException.class, () -> validator.validateIBAN("4545ABC4545")
+        );
+        assertEquals("Format is invalid!", e1.getMessage());
+
+        Throwable e2 = assertThrows(
+                RuntimeException.class, () -> validator.validateIBAN("BB004545454545")
+        );
+        assertEquals("Country is not in IBAN list!", e2.getMessage());
+
+        Throwable e3 = assertThrows(
+                RuntimeException.class, () -> validator.validateIBAN("IE29AIBK93112345678")
+        );
+        assertEquals("Length is incorrect!", e3.getMessage());
+
+        Throwable e4 = assertThrows(
+                RuntimeException.class, () -> validator.validateIBAN("BY13N11B3600900000002Z00AB00")
+        );
+        assertEquals("Structure is invalid!", e4.getMessage());
+
+        Throwable e5 = assertThrows(
+                RuntimeException.class, () -> validator.validateIBAN("BE68539607547032")
+        );
+        assertEquals("Number is invalid!", e5.getMessage());
+    }
+
+    @Test
+    void testConvertIBANToNumber() {
+        assertEquals("10111213", validator.convertIBANToNumber("ABCD"));
+        assertEquals("1011121356781234", validator.convertIBANToNumber("1234ABCD5678"));
+    }
+}
